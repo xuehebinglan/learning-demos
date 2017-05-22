cd68463aa176437c69e8265115ba31017b8b8061
xuehebinglan <785529534@qq.com>
2017-05-22 12:08:58 +0800

    fixed the webpack.config.js for webpack2.2

--- webpack/webpack-demo1/webpack.config.js
+++ webpack/webpack-demo1/webpack.config.js
@@ -1,7 +1,18 @@
+var path = require('path');
+
+
 module.exports = {
     entry: './src/script/main.js',
     output: {
-
-        filename: './dist/js/bundle.js'
+        path: path.resolve(__dirname, 'dist/js'),
+        filename: 'bundle.js'
     }
 }
+
+// module.exports = {
+//     entry: './src/script/main.js',
+//     output: {
+
+//         filename: './dist/js/bundle.js'
+//     }
+// }
